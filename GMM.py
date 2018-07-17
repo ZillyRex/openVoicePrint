fine GMM, still bug of overflow

GMM.py
@@ -15,22 +15,22 @@ def Gaussian(data, mean, cov):
 
 
 dim = 2
-N = 15
+N = 100
 data = np.random.rand(N, dim)*50
-K = 1
+K = 5
 P = np.array([1/K for i in range(K)])
-mean = np.zeros((K,dim))
+mean = [[] for k in range(K)]
 for k in range(K):
-    mean[k] = np.mean(data, axis=0)
+    mean[k] = np.mean(data, axis=0)+np.random.rand()*5
 cov = [0]*K
 for k in range(K):
     cov[k] = np.cov(data.T)
-threshold = 0.1
+threshold = 0.001
 likelyhood = 0
 likelyhood_old = 1
 # gamma is the probability that the nth data belongs to the kth Gaussian.
 gamma = np.array([np.zeros(K) for i in range(N)])
-while (abs(likelyhood-likelyhood_old) > threshold):
+while (np.abs(likelyhood-likelyhood_old) > threshold):
     likelyhood_old = likelyhood
 
     # E step
@@ -43,12 +43,14 @@ while (abs(likelyhood-likelyhood_old) > threshold):
     for k in range(K):
         Pk = np.sum([gamma[i][k] for i in range(N)])
         P[k] = Pk/N
-        mean[k] = np.sum([gamma[i][k]*data[i] for i in range(N)], axis=0)/Pk
+        mean[k] = (1.0/Pk)*np.sum([gamma[i][k]*data[i]
+                                   for i in range(N)], axis=0)
         data_diff = data-mean[k]
-        cov[k] = np.sum([gamma[i][k]*data_diff[i].reshape(dim, 1)*data_diff[i]
-                         for i in range(N)], axis=0)/Pk
+        cov[k] = (1.0/Pk)*np.sum([gamma[i][k]*data_diff[i].reshape(dim, 1)*data_diff[i]
+                                  for i in range(N)], axis=0)
 
-    likelyhood = np.sum([np.log(np.sum([P[k]*Gaussian(data[i], mean[k], cov[k])
-                                        for k in range(K)])) for i in range(N)])
-    print(mean)
+    likelyhood = np.sum([np.log(np.sum(
+        [P[k] * Gaussian(data[n], mean[k], cov[k]) for k in range(K)])) for n in range(N)])
+
+    # print(mean)
     print(likelyhood)